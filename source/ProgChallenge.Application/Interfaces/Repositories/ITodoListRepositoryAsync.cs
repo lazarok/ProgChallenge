342466fb1d7b410e9481554723ec2e5481a8d7da
Lázaro Kelsy Casaña Fernández <lazarokcf@hotmail.com>
2021-02-14 11:42:03 -0500

    Add TodoListRepositoryAsync

---
+++ source/ProgChallenge.Application/Interfaces/Repositories/ITodoListRepositoryAsync.cs
@@ -0,0 +1,14 @@
+﻿using ProgChallenge.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgChallenge.Application.Interfaces.Repositories
+{
+    public interface ITodoListRepositoryAsync : IGenericRepositoryAsync<TodoList>
+    {
+
+    }
+}
--- source/ProgChallenge.Infrastructure.Persistence/Repositories/TodoListRepositoryAsync.cs
+++ source/ProgChallenge.Infrastructure.Persistence/Repositories/TodoListRepositoryAsync.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using ProgChallenge.Application.Interfaces.Repositories;
+using ProgChallenge.Domain.Entities;
+using ProgChallenge.Infrastructure.Persistence.Contexts;
+using ProgChallenge.Infrastructure.Persistence.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +10,11 @@ using System.Threading.Tasks;
 
 namespace ProgChallenge.Infrastructure.Persistence.Repositories
 {
-    public class TodoListRepositoryAsync
+    public class TodoListRepositoryAsync : GenericRepositoryAsync<TodoList>, ITodoListRepositoryAsync
     {
+        public TodoListRepositoryAsync(ApplicationDbContext dbContext) : base(dbContext)
+        {
 
+        }
     }
 }

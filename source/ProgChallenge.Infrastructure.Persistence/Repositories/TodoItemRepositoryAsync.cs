864694f5fa992cf1c73ced2fb98cbc416e3f62a5
Lázaro Kelsy Casaña Fernández <lazarokcf@hotmail.com>
2021-02-14 11:39:38 -0500

    Remove IRepository instance of TodoItemRepositoryAsync

--- source/ProgChallenge.Infrastructure.Persistence/Repositories/TodoItemRepositoryAsync.cs
+++ source/ProgChallenge.Infrastructure.Persistence/Repositories/TodoItemRepositoryAsync.cs
@@ -8,11 +8,9 @@ namespace ProgChallenge.Infrastructure.Persistence.Repositories
 {
     public class TodoItemRepositoryAsync : GenericRepositoryAsync<TodoItem>, ITodoItemRepositoryAsync
     {
-        private readonly DbSet<TodoItem> _products;
-
         public TodoItemRepositoryAsync(ApplicationDbContext dbContext) : base(dbContext)
         {
-            _products = dbContext.Set<TodoItem>();
+
         }
     }
 }
